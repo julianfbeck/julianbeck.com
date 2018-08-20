39aec8a147491068548ba240b805dcdf9cf3a8da
Julian beck <ju-fa-beck@t-online.de>
2018-08-20 10:04:34 +0900

    changed form link

--- src/scenes/Contact/Contact.jsx
+++ src/scenes/Contact/Contact.jsx
@@ -6,13 +6,13 @@ export default class Contact extends Component {
     return (
       <div style={{ paddingTop: "4em" }} className="contact" >
         <Header size="huge">Contact me</Header>
-        <Form method="POST" action="https://formspree.io/Kickbeck@googlemail.com">
+        <Form className="contact" method="POST" netlify>
           <Form.Field>
             <label>Email</label>
             <input required type="email" name="email" placeholder="Your email" />
           </Form.Field>
           <Form.Field>
-            <Form.TextArea required label='message' name="message" className="message" placeholder='your Message' />
+            <Form.TextArea required label='Message' name="message" className="message" placeholder='your Message' />
           </Form.Field>
           <Button type='submit'>Submit</Button>
         </Form>
