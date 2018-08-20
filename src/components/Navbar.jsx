95d2654a81c46fafc9d99288e0e99bf721d37305
Julian beck <ju-fa-beck@t-online.de>
2018-08-20 12:58:43 +0900

    clean up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,40 +10,33 @@ import Blog from "../scenes/Blog/Blog";
 import Contact from "../scenes/Contact/Contact";
 import Entries from "../scenes/Entries/Entries";
 import ReactGA from "react-ga";
+
 ReactGA.initialize('UA-123121612-1');
 
-export function fireTracking(nextState) {
-
-  const { pathname } = nextState.location // this gives you the next URL
-
-  ReactGA.pageview(pathname)
-
-}
 
 export default class App extends Component {
   render() {
     return (
       <HashRouter>
         <div>
-        <Container>
-          <Menu pointing secondary size="large" >
-            <Menu.Item
-              name='Home' exact as={NavLink} to="/"
-            />
-            <Menu.Item
-              name='Blog' as={NavLink} to="/blog"
-            />
-            <Menu.Item
-              name='Contact' as={NavLink} to="/contact"
-            />
-          </Menu>
-        </Container>
-          <Container text>         
-            <Route onEnter={fireTracking} exact path="/" component={Home} />
+          <Container>
+            <Menu pointing secondary size="large" >
+              <Menu.Item
+                name='Home' exact as={NavLink} to="/"
+              />
+              <Menu.Item
+                name='Blog' as={NavLink} to="/blog"
+              />
+              <Menu.Item
+                name='Contact' as={NavLink} to="/contact"
+              />
+            </Menu>
+          </Container>
+          <Container text>
+            <Route exact path="/" component={Home} />
             <Route exact path="/blog" component={Blog} />
             <Route exact path="/blog/:id" component={Entries} />
             <Route path="/contact" component={Contact} />
-            
           </Container>
         </div>
       </HashRouter>
