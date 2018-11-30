2e3dd3440e42a4482bc021bc0573d49b81df00b3
Julian Beck <ju-fa-beck@t-online.de>
2018-11-30 09:46:56 +0100

    replaced image

--- src/main.js
+++ src/main.js
@@ -5,6 +5,11 @@ import store from "./store";
 import "./registerServiceWorker";
 import "../node_modules/bulma/css/bulma.min.css";
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {faGithub,faTwitter,faLinkedin}from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(faGithub,faLinkedin,faTwitter)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false;
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,10 +2,40 @@
   <div class="home">
     <div class="center">
       <figure class="image is-128x128 center">
-        <img class="is-rounded" src="https://media.licdn.com/dms/image/C5103AQGJzi-Kdahz4A/profile-displayphoto-shrink_200_200/0?e=1547078400&v=beta&t=trnWkHcDLZYPz7PGHjxhbm8Wpx-fYaWleMUiHYG1Gzo">
+        <img class="is-rounded" src="https://avatars2.githubusercontent.com/u/7285926?s=460&v=4">
       </figure>
-      <h1 class="title is-1">Julian Beck</h1>
+      <p class="title is-1">Julian Beck</p>
+      <p class="subtitle is-4">Computer Science Student</p>
+      <p >Intrested in full Stack development & languages</p>
+
     </div>
+    <div class="field has-addons has-text-centered">
+      <p class="control">
+        <a class="button is-medium">
+          <span class="icon">
+            <font-awesome-icon :icon="['fab', 'github']" />
+          </span>
+          <span>GitHub</span>
+        </a>
+      </p>
+      <p class="control">
+        <a class="button is-medium">
+          <span class="icon is-medium">
+            <font-awesome-icon :icon="['fab', 'twitter']" />
+          </span>
+          <span>Twitter</span>
+        </a>
+      </p>
+      <p class="control">
+        <a class="button is-medium">
+          <span class="icon is-medium">
+            <font-awesome-icon :icon="['fab', 'linkedin']" />
+          </span>
+          <span>linkedin</span>
+        </a>
+      </p>
+    </div>
+
   </div>
 </template>
 
