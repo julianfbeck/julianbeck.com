6e05816166a20b57e844be501ace5bf43b5c4723
Julian Beck <ju-fa-beck@t-online.de>
2019-02-24 22:23:50 +0100

    cleared up card

--- components/Card.vue
+++ components/Card.vue
@@ -1,26 +1,15 @@
 <template>
-  <li class="portfolio__item">
-    <nuxt-link 
-      :to="{ path: 'blogs/'+work.name, params: { slug: work.name }}"
-      class="portfolio__thumb-inner">
-      <div class="portfolio__thumb-hover" :style="backgroundColor">
-        <div class="portfolio__thumb-text">
-          <h3 class="portfolio__thumb-description" :class="{ 'portfolio__thumb-description--dark' : work.isTextColorDark }">
-            {{ work.title }}
-          </h3>
-          <h3 
-            class="portfolio__thumb-client" :class="{ 'portfolio__thumb-client--dark' : work.isTextColorDark }">
-            {{ work.description }}
-          </h3>
-        </div>
+  <li>
+    <nuxt-link :to="{ path: 'blogs/'+work.name, params: { slug: work.name }}">
+      <div class="">
+        <h3>
+          {{ work.title }}
+        </h3>
+        <h3>
+          {{ work.description }}
+        </h3>
       </div>
     </nuxt-link>
-    <div class="portfolio__description">
-      {{ work.title }}
-    </div>
-    <div class="portfolio__client">
-      {{ work.description }}
-    </div>
   </li>
 </template>
 
@@ -39,10 +28,7 @@
     },
 
     computed: {
-      backgroundColor () {
-        return `background: linear-gradient(to bottom, rgba(255, 255, 255, 0) 0%, ${this.work.color} 70%)`
-      }
+
     }
   }
 </script>
-
