5e645e108c9daa4e27d40dba927e06b38fece100
Julian Beck <ju-fa-beck@t-online.de>
2019-03-18 12:17:00 +0100

    added login actions to store

--- store/index.js
+++ store/index.js
@@ -19,9 +19,48 @@ const store = () => new Vuex.Store({
   },
 
   actions: {
-    // ...
+    nuxtServerInit ({ commit }, { req }) {
+        if (req.session && req.session.authUser) {
+          commit('SET_USER', req.session.authUser)
+        }
+    },
+    login ({ commit }, { username, password }) {
+        return fetch('/api/login', {
+          // Send the client cookies to the server
+          credentials: 'same-origin',
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            username,
+            password
+          })
+        })
+        .then((res) => {
+          if (res.status === 401) {
+            throw new Error('Bad credentials')
+          } else {
+            return res.json()
+          }
+        })
+        .then((authUser) => {
+          commit('SET_USER', authUser)
+        })
+    },
+    logout ({ commit }) {
+        return fetch('/api/logout', {
+          // Send the client cookies to the server
+          credentials: 'same-origin',
+          method: 'POST'
+        })
+        .then(() => {
+          commit('SET_USER', null)
+        })
+    }
   }
 
 })
+
 
 export default store
