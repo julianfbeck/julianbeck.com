5da2d3384c0bd526067d1e09a07210b57fe6090b
Julian Beck <ju-fa-beck@t-online.de>
2019-03-18 12:04:07 +0100

    minor

--- store/index.js
+++ store/index.js
@@ -1,19 +1,27 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+// Polyfill for `window.fetch()`
+require('whatwg-fetch')
+
 const store = () => new Vuex.Store({
 
-    state: () => ({
-      authUser: null
-    }),
-  
-    mutations: {
-      SET_USER: function (state, user) {
-        state.authUser = user
-      }
-    },
-  
-    actions: {
-      // ...
+  state: () => ({
+    authUser: null
+  }),
+
+  mutations: {
+    SET_USER: function (state, user) {
+      state.authUser = user
     }
-  
-  })
-  
-  export default store
+  },
+
+  actions: {
+    // ...
+  }
+
+})
+
+export default store
