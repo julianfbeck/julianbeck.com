b1475e241232a30d6b6c7c5cd4d0b8a166254c67
Julian Beck <ju-fa-beck@t-online.de>
2019-04-08 22:15:35 +0200

    testing loaclhost

--- server/index.js
+++ server/index.js
@@ -57,7 +57,7 @@ async function start() {
     res.status(401).send(err + '')
   })
   // Listen the server
-  app.listen(port)
+  app.listen(port,"localhost")
   console.log('Server listening on http://' + host + ':' + port) // eslint-disable-line no-console
 }
 start()
