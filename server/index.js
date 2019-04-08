e3dc7b9ffe82d16e2a3228f887e38ceee75d1bac
Julian Beck <ju-fa-beck@t-online.de>
2019-04-08 22:18:30 +0200

    chaning back to host

--- server/index.js
+++ server/index.js
@@ -57,7 +57,7 @@ async function start() {
     res.status(401).send(err + '')
   })
   // Listen the server
-  app.listen(port,"localhost")
+  app.listen(port,host)
   console.log('Server listening on http://' + host + ':' + port) // eslint-disable-line no-console
 }
 start()
