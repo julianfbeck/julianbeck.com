c8c626496f2b3c7f9d200f0297fba9b2284c1d92
Julian <ju-fa-beck@t-online.de>
2022-02-17 08:36:55 +0100

    Update gtag.ts

--- components/gtag.ts
+++ components/gtag.ts
@@ -2,9 +2,11 @@ export const GA_TRACKING_ID = "G-2EZECF8VMG";
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL): void => {
+  if (typeof window.gtag !== 'undefined' {
   window.gtag("config", GA_TRACKING_ID, {
     page_path: url,
   });
+}
 };
 
 type GTagEvent = {
