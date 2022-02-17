7f941e963072876a4adda23f3552c37dc034363b
Julian <ju-fa-beck@t-online.de>
2022-02-17 08:30:25 +0100

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,13 +19,16 @@ export default class MyDocument extends Document {
               <script
                 // eslint-disable-next-line react/no-danger
                 dangerouslySetInnerHTML={{
-                  __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
+                  __html: `if (typeof window !== 'undefined') {
+               
+
+                      window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+                      gtag('config', '${GA_TRACKING_ID}', {
+                        page_path: window.location.pathname,
+                      });
+             }
           `,
                 }}
               />
