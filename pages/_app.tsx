6d43464ba4768f1cad242608f8a8a46e63f1404e
Julian Beck <ju-fa-beck@t-online.de>
2022-10-13 14:48:10 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import "../styles/index.css";
 import { AppProps } from "next/app";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
-import * as gtag from "../components/gtag";
 import PlausibleProvider from "next-plausible";
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
