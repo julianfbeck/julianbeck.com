1e171017c8caf4f727bd006c994654a9c7d9b119
Julian Beck <ju-fa-beck@t-online.de>
2024-01-12 19:38:25 +0100

    Fix Project Component

--- astro-2.0/src/components/react/Project.tsx
+++ astro-2.0/src/components/react/Project.tsx
@@ -12,47 +12,48 @@ export const Project: React.FC<{
   color: string;
   description: string;
   url: string;
-  tags: string
+  tags: string;
 }> = ({ name, url, logo, color, description, tags }) => {
   const { scrollYProgress } = useViewportScroll();
   const scale = useTransform(scrollYProgress, [0, 1], [1, 1]);
   return (
-    <motion.a
-      href={url}
-      target="_blank"
-      variants={{
-        hidden: { filter: "grayscale(80%)" },
-        visible: {
-          transition: { duration: 0.4, easings: "circOut" },
-          opacity: 1,
-          filter: "grayscale(0%)",
-          scale: 1,
-        },
-      }}
-      className="rounded-lg shadow-md shadow-gray-600/60 bg-gray-800 cursor-pointer overflow-hidden transition-all duration-200 group hover:shadow-lg"
-    >
-      <div
-        className="h-32 w-full flex justify-center items-center transition-all duration-200 transform"
-        style={{ backgroundColor: color }}
+    <a href={url} target="_blank">
+      <motion.div
+        // target="_blank"
+        className="rounded-lg shadow-md shadow-blue-800 bg-gray-800 cursor-pointer overflow-hidden transition-all duration-200 group hover:shadow-lg"
+        variants={{
+          hidden: { filter: "grayscale(80%)" },
+          visible: {
+            transition: { duration: 0.4, easings: "circOut" },
+            opacity: 1,
+            filter: "grayscale(0%)",
+            scale: 1,
+          },
+        }}
       >
-        <img
-          class="w-16 transition-transform rounded-lg duration-500 transform group-hover:scale-110"
-          src={logo}
-          alt={name}
-        />
-      </div>
-      <div className="p-4">
-        <div className="flex items-center">
-          <h3 className="text-lg font-semibold text-white">{name}</h3>
+        <div
+          className="h-32 w-full flex justify-center items-center transition-all duration-200 transform"
+          style={{ backgroundColor: color }}
+        >
           <img
-            className="ml-auto opacity-0 transform duration-200 group-hover:opacity-100 h-3"
-            src="./arrow-right.svg"
+            className="w-16 transition-transform rounded-lg duration-500 transform group-hover:scale-110"
+            src={logo}
+            alt={name}
           />
         </div>
+        <div className="p-4">
+          <div className="flex items-center">
+            <h3 className="text-lg font-semibold text-white">{name}</h3>
+            <img
+              className="ml-auto opacity-0 transform duration-200 group-hover:opacity-100 h-3"
+              src="./arrow-right.svg"
+            />
+          </div>
 
-        <div className="mt-3 text-sm text-white">{description}</div>
-        {tags}
-      </div>
-    </motion.a>
+          <div className="mt-3 text-sm text-white">{description}</div>
+          {tags}
+        </div>
+      </motion.div>
+    </a>
   );
 };
