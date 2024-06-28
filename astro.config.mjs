6d05394d7b09f6e246df47f046f137fea1e27c92
Julian Beck <ju-fa-beck@t-online.de>
2024-06-28 22:40:10 +0200

    chore: Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://juli.sh",
+  site: "https://julianbeck.com",
 
   integrations: [
     sitemap(),
